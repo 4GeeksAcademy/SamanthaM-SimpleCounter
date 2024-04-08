6e83f5abfd9ed26b9f1373d40943066185fa0b1e
Samantha McCabe <156986930+SamanthaMcCabe2104@users.noreply.github.com>
2024-04-08 23:21:27 +0000

    done

---
+++ src/js/component/icons.js
@@ -0,0 +1,17 @@
+import fontawesome from '@fortawesome/fontawesome';
+
+import faClock from '@fortawesome/fontawesome-free-regular/faClock';
+
+fontawesome.config = {
+    autoReplaceSvg: 'nest'
+}
+
+fontawesome.library.add(faClock);import fontawesome from '@fortawesome/fontawesome';
+
+import faClock from '@fortawesome/fontawesome-free-regular/faClock';
+
+fontawesome.config = {
+    autoReplaceSvg: 'nest'
+}
+
+fontawesome.library.add(faClock);
